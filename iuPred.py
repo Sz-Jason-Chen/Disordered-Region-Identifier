e9047a3429369306f4179c29d6ba800425377038
mingshi1 <101240207+mingshi1@users.noreply.github.com>
2023-12-13 21:38:47 +0800

    add pseudocode

--- iuPred.py
+++ iuPred.py
@@ -7,58 +7,14 @@ import string
 import math
 from cores import *
 from scipy.interpolate import make_interp_spline
-# input: String: 'P06400', 'Q7L5Y9', 'Q15326', or 'Q92793'
-# output: A comparasion plot between our score and IUPred3 score. Sites with score larger than 0.5
-#         are considered disordered.
-def iuPred(UID="Q92793"):
-    # amino acid interaction energy matrix
-    P=[ [-1.65, -2.83, 1.16, 1.80, -3.73, -0.41, 1.90, -3.69, 0.49, -3.01, -2.08, 0.66, 1.54, 1.20, 0.98, -0.08, 0.46, -2.31, 0.32,-4.62],
-        [-2.83, -39.58, -0.82, -0.53, -3.07, -2.96, -4.98, 0.34, -1.38, -2.15, 1.43, -4.18, -2.13, -2.91, -0.41, -2.33, -1.84, -0.16, 4.26, -4.46],
-        [1.16, -0.82, 0.84, 1.97, -0.92, 0.88, -1.07, 0.68, -1.93, 0.23, 0.61, 0.32, 3.31, 2.67, -2.02, 0.91, -0.65, 0.94, -0.71, 0.90],
-        [1.80, -0.53, 1.97, 1.45, 0.94, 1.31, 0.61, 1.30, -2.51, 1.14, 2.53, 0.20, 1.44, 0.10, -3.13, 0.81, 1.54, 0.12, -1.07, 1.29],
-        [-3.73, -3.07, -0.92, 0.94, -11.25, 0.35, -3.57, -5.88, -0.82, -8.59, -5.34, 0.73, 0.32, 0.77, -0.40, -2.22, 0.11, -7.05, -7.09, -8.80],
-        [-0.41, -2.96, 0.88, 1.31, 0.35, -0.20, 1.09, -0.65, -0.16, -0.55, -0.52, -0.32, 2.25, 1.11, 0.84, 0.71, 0.59, -0.38, 1.69, -1.90],
-        [1.90, -4.98, -1.07, 0.61, -3.57, 1.09, 1.97, -0.71, 2.89, -0.86, -0.75, 1.84, 0.35, 2.64, 2.05, 0.82, -0.01, 0.27, -7.58, -3.20],
-        [-3.69, 0.34, 0.68, 1.30, -5.88, -0.65, -0.71, -6.74, -0.01, -9.01, -3.62, -0.07, 0.12, -0.18, 0.19, -0.15, 0.63, -6.54, -3.78, -5.26],
-        [0.49, -1.38, -1.93, -2.51, -0.82, -0.16, 2.89, -0.01, 1.24, 0.49, 1.61, 1.12, 0.51, 0.43, 2.34, 0.19, -1.11, 0.19, 0.02, -1.19],
-        [-3.01, -2.15, 0.23, 1.14, -8.59, -0.55, -0.86, -9.01, 0.49, -6.37, -2.88, 0.97, 1.81, -0.58, -0.60, -0.41, 0.72, -5.43, -8.31,-4.90],
-        [-2.08, 1.43, 0.61, 2.53, -5.34, -0.52, -0.75, -3.62, 1.61, -2.88, -6.49, 0.21, 0.75, 1.90, 2.09, 1.39, 0.63, -2.59, -6.88, -9.73],
-        [0.66, -4.18, 0.32, 0.20, 0.73, -0.32, 1.84, -0.07, 1.12, 0.97, 0.21, 0.61, 1.15, 1.28, 1.08, 0.29, 0.46, 0.93, -0.74, 0.93],
-        [1.54, -2.13, 3.31, 1.44, 0.32, 2.25, 0.35, 0.12, 0.51, 1.81, 0.75, 1.15, -0.42, 2.97, 1.06, 1.12, 1.65, 0.38, -2.06, -2.09],
-        [1.20, -2.91, 2.67, 0.10, 0.77, 1.11, 2.64, -0.18, 0.43, -0.58, 1.90, 1.28, 2.97, -1.54, 0.91, 0.85, -0.07, -1.91, -0.76, 0.01],
-        [0.98, -0.41, -2.02, -3.13, -0.40, 0.84, 2.05, 0.19, 2.34, -0.60, 2.09, 1.08, 1.06, 0.91, 0.21, 0.95, 0.98, 0.08, -5.89, 0.36],
-        [-0.08, -2.33, 0.91, 0.81, -2.22, 0.71, 0.82, -0.15, 0.19, -0.41, 1.39, 0.29, 1.12, 0.85, 0.95, -0.48, -0.06, 0.13, -3.03, -0.82],
-        [0.46, -1.84, -0.65, 1.54, 0.11, 0.59, -0.01, 0.63, -1.11, 0.72, 0.63, 0.46, 1.65, -0.07, 0.98, -0.06, -0.96, 1.14, -0.65, -0.37],
-        [-2.31, -0.16, 0.94, 0.12, -7.05, -0.38, 0.27, -6.54, 0.19, -5.43, -2.59, 0.93, 0.38, -1.91, 0.08, 0.13, 1.14, -4.82, -2.13, -3.59],
-        [0.32, 4.26, -0.71, -1.07, -7.09, 1.69, -7.58, -3.78, 0.02, -8.31, -6.88, -0.74, -2.06, -0.76, -5.89, -3.03, -0.65, -2.13, -1.73, -12.39],
-        [-4.62, -4.46, 0.90, 1.29, -8.80, -1.90, -3.20, -5.26, -1.19, -4.90, -9.73, 0.93, -2.09, 0.01, 0.36,-0.82,-0.37,-3.59,-12.39,-2.68]]
-    amino_acids = {
-        "ALA": "A",
-        "ARG": "R",
-        "ASN": "N",
-        "ASP": "D",
-        "CYS": "C",
-        "GLU": "E",
-        "GLN": "Q",
-        "GLY": "G",
-        "HIS": "H",
-        "ILE": "I",
-        "LEU": "L",
-        "LYS": "K",
-        "MET": "M",
-        "PHE": "F",
-        "PRO": "P",
-        "SER": "S",
-        "THR": "T",
-        "TRP": "W",
-        "TYR": "Y",
-        "VAL": "V",
-    }
-    aa_name=["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"]
+from matplotlib.patches import Rectangle
 
-    # Set example test data
-    # input: String: 'P06400', 'Q7L5Y9', 'Q15326', or 'Q92793'
-    def example(UID):
+# Set example test data
+def example(UID):
+        """
+        :param UID: Accession number of Uniprot database, should be 'P06400', 'Q7L5Y9', 'Q15326', or 'Q92793'.
+        :return: 
+        """
         global aa_seq
         global IUP_ref
         aa_seq=get_amino_sequence(UID)
@@ -4440,6 +4396,61 @@ def iuPred(UID="Q92793"):
     ])
         else:
             print("The input of example function should be 'P29590', 'P06400', 'Q7L5Y9', 'Q15326', or 'Q92793'. ")
+# input: String: 'P06400', 'Q7L5Y9', 'Q15326', or 'Q92793'
+# output: A comparasion plot between our score and IUPred3 score. Sites with score larger than 0.5
+#         are considered disordered.
+def iuPred(UID="Q92793"):
+    """
+    output: A comparasion plot between our score and IUPred3 score. Sites with score larger than 0.5 are considered disordered.
+    
+    :param UID: Accession number of Uniprot database, should be 'P06400', 'Q7L5Y9', 'Q15326', or 'Q92793'.
+    :return: 
+    """
+    # amino acid interaction energy matrix
+    P=[ [-1.65, -2.83, 1.16, 1.80, -3.73, -0.41, 1.90, -3.69, 0.49, -3.01, -2.08, 0.66, 1.54, 1.20, 0.98, -0.08, 0.46, -2.31, 0.32,-4.62],
+        [-2.83, -39.58, -0.82, -0.53, -3.07, -2.96, -4.98, 0.34, -1.38, -2.15, 1.43, -4.18, -2.13, -2.91, -0.41, -2.33, -1.84, -0.16, 4.26, -4.46],
+        [1.16, -0.82, 0.84, 1.97, -0.92, 0.88, -1.07, 0.68, -1.93, 0.23, 0.61, 0.32, 3.31, 2.67, -2.02, 0.91, -0.65, 0.94, -0.71, 0.90],
+        [1.80, -0.53, 1.97, 1.45, 0.94, 1.31, 0.61, 1.30, -2.51, 1.14, 2.53, 0.20, 1.44, 0.10, -3.13, 0.81, 1.54, 0.12, -1.07, 1.29],
+        [-3.73, -3.07, -0.92, 0.94, -11.25, 0.35, -3.57, -5.88, -0.82, -8.59, -5.34, 0.73, 0.32, 0.77, -0.40, -2.22, 0.11, -7.05, -7.09, -8.80],
+        [-0.41, -2.96, 0.88, 1.31, 0.35, -0.20, 1.09, -0.65, -0.16, -0.55, -0.52, -0.32, 2.25, 1.11, 0.84, 0.71, 0.59, -0.38, 1.69, -1.90],
+        [1.90, -4.98, -1.07, 0.61, -3.57, 1.09, 1.97, -0.71, 2.89, -0.86, -0.75, 1.84, 0.35, 2.64, 2.05, 0.82, -0.01, 0.27, -7.58, -3.20],
+        [-3.69, 0.34, 0.68, 1.30, -5.88, -0.65, -0.71, -6.74, -0.01, -9.01, -3.62, -0.07, 0.12, -0.18, 0.19, -0.15, 0.63, -6.54, -3.78, -5.26],
+        [0.49, -1.38, -1.93, -2.51, -0.82, -0.16, 2.89, -0.01, 1.24, 0.49, 1.61, 1.12, 0.51, 0.43, 2.34, 0.19, -1.11, 0.19, 0.02, -1.19],
+        [-3.01, -2.15, 0.23, 1.14, -8.59, -0.55, -0.86, -9.01, 0.49, -6.37, -2.88, 0.97, 1.81, -0.58, -0.60, -0.41, 0.72, -5.43, -8.31,-4.90],
+        [-2.08, 1.43, 0.61, 2.53, -5.34, -0.52, -0.75, -3.62, 1.61, -2.88, -6.49, 0.21, 0.75, 1.90, 2.09, 1.39, 0.63, -2.59, -6.88, -9.73],
+        [0.66, -4.18, 0.32, 0.20, 0.73, -0.32, 1.84, -0.07, 1.12, 0.97, 0.21, 0.61, 1.15, 1.28, 1.08, 0.29, 0.46, 0.93, -0.74, 0.93],
+        [1.54, -2.13, 3.31, 1.44, 0.32, 2.25, 0.35, 0.12, 0.51, 1.81, 0.75, 1.15, -0.42, 2.97, 1.06, 1.12, 1.65, 0.38, -2.06, -2.09],
+        [1.20, -2.91, 2.67, 0.10, 0.77, 1.11, 2.64, -0.18, 0.43, -0.58, 1.90, 1.28, 2.97, -1.54, 0.91, 0.85, -0.07, -1.91, -0.76, 0.01],
+        [0.98, -0.41, -2.02, -3.13, -0.40, 0.84, 2.05, 0.19, 2.34, -0.60, 2.09, 1.08, 1.06, 0.91, 0.21, 0.95, 0.98, 0.08, -5.89, 0.36],
+        [-0.08, -2.33, 0.91, 0.81, -2.22, 0.71, 0.82, -0.15, 0.19, -0.41, 1.39, 0.29, 1.12, 0.85, 0.95, -0.48, -0.06, 0.13, -3.03, -0.82],
+        [0.46, -1.84, -0.65, 1.54, 0.11, 0.59, -0.01, 0.63, -1.11, 0.72, 0.63, 0.46, 1.65, -0.07, 0.98, -0.06, -0.96, 1.14, -0.65, -0.37],
+        [-2.31, -0.16, 0.94, 0.12, -7.05, -0.38, 0.27, -6.54, 0.19, -5.43, -2.59, 0.93, 0.38, -1.91, 0.08, 0.13, 1.14, -4.82, -2.13, -3.59],
+        [0.32, 4.26, -0.71, -1.07, -7.09, 1.69, -7.58, -3.78, 0.02, -8.31, -6.88, -0.74, -2.06, -0.76, -5.89, -3.03, -0.65, -2.13, -1.73, -12.39],
+        [-4.62, -4.46, 0.90, 1.29, -8.80, -1.90, -3.20, -5.26, -1.19, -4.90, -9.73, 0.93, -2.09, 0.01, 0.36,-0.82,-0.37,-3.59,-12.39,-2.68]]
+    amino_acids = {
+        "ALA": "A",
+        "ARG": "R",
+        "ASN": "N",
+        "ASP": "D",
+        "CYS": "C",
+        "GLU": "E",
+        "GLN": "Q",
+        "GLY": "G",
+        "HIS": "H",
+        "ILE": "I",
+        "LEU": "L",
+        "LYS": "K",
+        "MET": "M",
+        "PHE": "F",
+        "PRO": "P",
+        "SER": "S",
+        "THR": "T",
+        "TRP": "W",
+        "TYR": "Y",
+        "VAL": "V",
+    }
+    aa_name=["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"]
+
     example(UID)
 
     # Use amino acid interaction energy matrix to predict probability of every site to be disorder.
@@ -4496,4 +4507,126 @@ def iuPred(UID="Q92793"):
     plt.axhline(y=0.5, color='red')
     plt.show()
 
-iuPred('Q92793')
+iuPred('P06400')
+
+
+""" def draw_IUPred3(protein):
+    
+    sequence = get_amino_sequence(protein)  # AA sequence
+    if sequence is None:
+        print('No available protein sequence.')
+        return -1
+    
+    # get all structure files
+    print('Accessing PDB files')
+    pdb_params = get_pdb_params(protein=protein)
+    file_paths = access_protein_files(protein=protein)
+    print('File validation complete')
+
+    print('----------------------------------------')
+    print('Identifying gaps')
+    valid_sections = []
+    af_score = None  # AF score (100 - pLDDT)
+    gaps_matrices = None
+    # read every structure files
+    for path in file_paths:
+        # AF file
+        if path.split('.')[-2][-9:] == 'alphafold':
+            print(f'Resolving AlphaFold file')
+            alpha_fold_reader = AlphaFoldReader(file_path=path)
+            af_score = 100 - np.array(alpha_fold_reader.get_scores())  # 100 - pLDDT
+        # real measurement files
+        elif path.split('.')[-2][-9:] != 'alphafold':
+            print(f'Examining {path[-8:-4].upper()} file')
+            reader = Reader(file_path=path, params=pdb_params[path.split('.')[-2][-4:]])
+            resolution = reader.get_resolution()
+            # ignore structure without resolution (cannot be scored)
+            if resolution:
+                gap_array = reader.get_potential_gaps(sequence=sequence)
+                if gaps_matrices is not None:
+                    gaps_matrices = np.vstack((gaps_matrices, gap_array))
+                else:
+                    gaps_matrices = gap_array
+                valid_sections.append(reader.get_valid_sections())
+
+    #print(gaps_matrices)
+
+    # testing outputs
+    print('----------------------------------------')
+    print('Scoring')
+    if gaps_matrices is None:
+        print('No available PDB file for gap identification.')
+        return 1
+    # scoring
+    # score_matrix = multi_chain_scoring(identifiers=identifiers, sequence=sequence, resolutions=resolutions, gaps=gaps, valid_sections=valid_sections, pdb_params=pdb_params)
+    score_matrix = scoring(gaps_matrices=gaps_matrices)
+    #print(score_matrix)
+
+
+    print('----------------------------------------')
+    print('Result')
+    
+    # merge the score with the AF prediction
+    final_matrix = np.vstack((score_matrix, af_score))
+    table = pd.DataFrame(final_matrix, index=['Score', 'AlphaFold'])
+    # Convert table from index 0 to index 1
+    table = table.rename(columns=dict(zip(table.columns, table.columns + 1)))
+
+    table.loc[2]=IUP_ref*100
+    print(table.shape[1])
+    print(len(IUP_ref))
+    gap_positions = []
+    start = 0
+    avg_score = 0
+    af_avg_score = 0
+    for column_name, value in table.loc['Score'].items():
+        if value >= 50 and start == 0:
+            start = column_name
+            avg_score = value
+            af_avg_score = table.loc['AlphaFold', column_name]
+        elif value >= 50 and start != 0 and column_name != len(sequence):
+            avg_score += value
+            af_avg_score += table.loc['AlphaFold', column_name]
+        elif value < 50 and start != 0:
+            end = column_name - 1
+            avg_score /= end - start + 1
+            af_avg_score /= end - start + 1
+            gap_positions.append({'start': start, 'end': end, 'avg_score': avg_score, 'af_avg_score':af_avg_score})
+            start = 0
+            af_avg_score = 0
+        elif value >= 50 and start != 0 and column_name == len(sequence):
+            end = len(sequence)
+            avg_score += value
+            af_avg_score += table.loc['AlphaFold', column_name]
+            avg_score /= end - start + 1
+            af_avg_score /= end - start + 1
+            gap_positions.append({'start': start, 'end': end, 'avg_score': avg_score, 'af_avg_score':af_avg_score})
+        elif value >= 50 and start == 0 and column_name == len(sequence):
+            gap_positions.append({'start': len(sequence), 'end': len(sequence), 'avg_score': value, 'af_avg_score':table.loc['AlphaFold', column_name]})
+    # print(gap_positions)
+
+
+
+    if len(gap_positions) > 0:
+        print(f'{len(gap_positions)} gaps are identified.')
+        print('The gaps and their average position scores are listed:')
+        for gap in gap_positions:
+            print(f'Start: {gap["start"]},\tEnd: {gap["end"]},\tAvg score: {round(gap["avg_score"], 2)},\tAF avg score: {round(gap["af_avg_score"], 2)}')
+    else:
+        print('No gap identified')
+
+    print('The heatmap presents each position\'s score, and the gap regions are marked with red boxes.')
+    # heatmap output
+    sns.heatmap(table, cmap='viridis', vmin=0, vmax=100)
+    for gap in gap_positions:
+        rectangle = Rectangle((gap["start"], 0), width=(gap["end"]-gap["start"]+1), height=2, linewidth=1, edgecolor='red', facecolor='none')
+        plt.gca().add_patch(rectangle)
+    plt.title(f'{protein}   (n={len(gaps_matrices)})')
+    plt.show()
+    
+    return 0
+
+
+draw_IUPred3('P06400') """
+
+
